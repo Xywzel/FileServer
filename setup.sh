6a16baa325d1f317f747a60f218278e3a6df6f3f
Xywzel <oskari.nousiainen@iki.fi>
2022-05-17 12:20:20 +0300

    Added initial setup script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+# Python 3 is required, I used 3.10.4
+python3 --version
+# Pip for python 3 is required, I used 22.0.2
+pip --version
+# Without other containers used, venv is recommended
+# to not mesh dependencies when swapping projects
+python3 -m venv venv
+source venv/bin/activate
+
+# Installing Django and other requirements
+python -m pip install Django
+# Check the version used, I had 4.0.4
+django-admin --version
+
+# Install Django REST framework
+python -m pip install djangorestframework
+
